2762487d370f2981280a0083862466a2886a1c2b
black-code1 <legrandmahamat1@gmail.com>
2021-07-07 15:57:02 +0100

    Added image, metadata and titles with the help of Image and Head component

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,19 @@
+import Head from 'next/head'
 const About = () => {
 
   return (
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
-    </div>
+    <>
+      <Head>
+          <title>coder List | About</title>
+          <meta name="keywords" content="coders"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
+      </div>
+    </>
   )
 }
 
