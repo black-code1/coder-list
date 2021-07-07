30f3bc0abab9f7bae6b3098c8d90dc6b4ab71ec4
black-code1 <legrandmahamat1@gmail.com>
2021-07-07 13:30:07 +0100

    linking between pages by importing link from next/link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
   return (  
     <nav>
       <div className="logo">
         <h1>Coder List</h1>
       </div>
-      <a>Home</a>
-      <a>About</a>
-      <a>Coder Listing</a>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/coders"><a>Coder Listing</a></Link>
     </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
@@ -11,6 +12,8 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates modi nam numquam excepturi odio quia iusto sed voluptatibus quae? Ullam nisi temporibus tenetur. Recusandae nesciunt neque, enim repellendus corrupti dolores.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates modi nam numquam excepturi odio quia iusto sed voluptatibus quae? Ullam nisi temporibus tenetur. Recusandae nesciunt neque, enim repellendus corrupti dolores.</p>
+      
+      <Link href="/coders/"><a>See Coder Listing</a></Link>
       <Footer />
     </div>
   )
