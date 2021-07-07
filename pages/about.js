c4c5f3c78eefd9e123eefe35deee8c1aecca4ddd
black-code1 <legrandmahamat1@gmail.com>
2021-07-07 12:07:59 +0100

    created my first component and sub folder component

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+const About = () => {
+
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate asperiores quasi at commodi, quas inventore ipsum accusantium repellendus officiis dolorem voluptates debitis sapiente sequi, blanditiis ex. Ex, fuga magnam?</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ pages/coders/index.js
@@ -0,0 +1,9 @@
+const Coders = () => {
+  return (  
+    <div>
+      <h1>All Coders</h1>
+    </div>
+  );
+}
+ 
+export default Coders;
