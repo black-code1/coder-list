a248fcba0a67cd921ec17938418769270447f3cc
black-code1 <legrandmahamat1@gmail.com>
2021-07-07 16:54:26 +0100

    Fetched data from an api with getStaticProps provided by nextjs

--- pages/coders/index.js
+++ pages/coders/index.js
@@ -1,7 +1,29 @@
-const Coders = () => {
+import styles from '../../styles/Jobs.module.css'
+
+export const getStaticProps = async () => {
+
+  const response = await fetch('https://api.github.com/users');
+  const data = await response.json();
+
+  return {
+    props: { coders: data }
+  }
+}
+
+
+const Coders = ({ coders }) => {
+  console.log(coders)
+
   return (  
     <div>
       <h1>All Coders</h1>
+      {coders.map(coder => (
+        <div key={coder.id}>
+          <a className={styles.single}>
+            <h3>{ coder.login }</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
