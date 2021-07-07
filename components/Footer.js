b7e6df1433bfe627cc8f1b41dce599b7b7161b02
black-code1 <legrandmahamat1@gmail.com>
2021-07-07 12:27:03 +0100

    created a components folder at the root of the app containing the Navbar and Footer component

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return ( 
+    <footer>
+      Copyright &copy; 2021 Coder List
+    </footer>
+   );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates modi nam numquam excepturi odio quia iusto sed voluptatibus quae? Ullam nisi temporibus tenetur. Recusandae nesciunt neque, enim repellendus corrupti dolores.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates modi nam numquam excepturi odio quia iusto sed voluptatibus quae? Ullam nisi temporibus tenetur. Recusandae nesciunt neque, enim repellendus corrupti dolores.</p>
+      <Footer />
     </div>
   )
 }
